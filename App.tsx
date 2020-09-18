59b894c9884b3e8a67070e21f98f3e2fe58d2001
marscarda <52551131+marscarda@users.noreply.github.com>
2020-09-18 05:01:11 -0300

    App Created

--- App.tsx
+++ App.tsx
@@ -1,16 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>App Created</Text>
+      <Text>First Commit</Text>
+      </View>
+    );
+/*
+Keep it withou deleting it until you remember what it is and how to use it.
+var clearText = () => {
+   _textInput.setNativeProps({text: ''});
 }
+return (
+  <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <Text>App Created</Text>
+    <Text>First Commit</Text>
+    <TextInput
+      ref={component => _textInput = component}
+      style={{height: 50, width: 200, marginHorizontal: 20, borderWidth: 1, borderColor: '#ccc'}}
+    />
+    <TouchableOpacity onPress={clearText}>
+      <Text>Clear text</Text>
+    </TouchableOpacity>
+  </View>
+);
+*/
 
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
